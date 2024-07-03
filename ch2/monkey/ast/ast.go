a63e0b2f0a1d90bab6b1cffc0d498538acb4802a
KhaledKorfali2 <celine.kor7@gmail.com>
2024-07-03 14:49:01 -0700

    function literal parsing added

--- ch2/monkey/ast/ast.go
+++ ch2/monkey/ast/ast.go
@@ -3,7 +3,9 @@ package ast
 
 import (
 	"monkey/token"
-	"bytes")
+	"bytes"
+	"strings"
+)
 
 type Node interface {
 	TokenLiteral() string
@@ -221,6 +223,31 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else")
 		out.WriteString(ie.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token	token.Token // the 'fn' token
+	Parameters	[]*Identifier
+	Body	*BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Body.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
