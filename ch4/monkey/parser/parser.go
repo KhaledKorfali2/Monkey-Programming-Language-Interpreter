248b107e476d352f35d00692b44a783814628801
KhaledKorfali2 <celine.kor7@gmail.com>
2024-09-08 11:22:48 -0700

    added parsing array literals

--- ch4/monkey/parser/parser.go
+++ ch4/monkey/parser/parser.go
@@ -68,7 +68,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
-	
+	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+
 	p.registerPrefix(token.IF, p.parseIfExpression)
 	
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
@@ -95,33 +96,33 @@ func New(l *lexer.Lexer) *Parser {
 
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	exp := &ast.CallExpression{Token: p.curToken, Function: function}
-	exp.Arguments = p.parseCallArguments()
+	exp.Arguments = p.parseExpressionList(token.RPAREN)
 	return exp
 }
 
-func (p *Parser) parseCallArguments() []ast.Expression {
-	args := []ast.Expression{}
-
-	if p.peekTokenIs(token.RPAREN) {
-		p.nextToken()
-		return args
-	}
-
-	p.nextToken()
-	args = append(args, p.parseExpression(LOWEST))
-
-	for p.peekTokenIs(token.COMMA) {
-		p.nextToken()
-		p.nextToken()
-		args = append(args, p.parseExpression(LOWEST))
-	}
-
-	if !p.expectPeek(token.RPAREN) {
-		return nil
-	}
-
-	return args
-}
+//func (p *Parser) parseCallArguments() []ast.Expression {
+//	args := []ast.Expression{}
+//
+//	if p.peekTokenIs(token.RPAREN) {
+//		p.nextToken()
+//		return args
+//	}
+//
+//	p.nextToken()
+//	args = append(args, p.parseExpression(LOWEST))
+//
+//	for p.peekTokenIs(token.COMMA) {
+//		p.nextToken()
+//		p.nextToken()
+//		args = append(args, p.parseExpression(LOWEST))
+//	}
+//
+//	if !p.expectPeek(token.RPAREN) {
+//		return nil
+//	}
+//
+//	return args
+//}
 
 func (p *Parser) parseFunctionLiteral() ast.Expression {
 	lit := &ast.FunctionLiteral{Token: p.curToken}
@@ -398,6 +399,38 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseArrayLiteral() ast.Expression {
+	array := &ast.ArrayLiteral{Token: p.curToken}
+
+	array.Elements = p.parseExpressionList(token.RBRACKET)
+
+	return array
+}
+
+func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
+	list := []ast.Expression{}
+
+	if p.peekTokenIs(end) {
+		p.nextToken()
+		return list
+	}
+
+	p.nextToken()
+	list = append(list, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		list = append(list, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(end) {
+		return nil
+	}
+
+	return list
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
