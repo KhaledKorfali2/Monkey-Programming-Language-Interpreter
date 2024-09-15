b4e98812fe8dbe938fe4343a20c58da487739002
KhaledKorfali2 <celine.kor7@gmail.com>
2024-09-14 17:18:30 -0700

    began implementing hash objects

--- ch4/monkey/object/object.go
+++ ch4/monkey/object/object.go
@@ -7,7 +7,9 @@ import (
 	"fmt"
 	"monkey/ast"
 	"strings"
+	"hash/fnv"
 )
+
 type ObjectType string
 
 const (
@@ -126,3 +128,31 @@ func (ao *Array) Inspect() string {
 	return out.String()
 }
 func (ao *Array) Type() ObjectType { return ARRAY_OBJ}
+
+type HashKey struct {
+	Type ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
